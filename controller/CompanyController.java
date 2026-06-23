a6f40e97f10f915a84f415c578e1559bc8be137b
Murar Ioana <murarioana2005@icloud.com>
2026-06-23 12:38:48 +0300

    Initial project + ziua 2

---
+++ controller/CompanyController.java
@@ -0,0 +1,35 @@
+package com.company.booking.controller;
+
+import com.company.booking.dto.request.CreateCompanyRequest;
+import com.company.booking.dto.response.CompanyResponse;
+import com.company.booking.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/companies")
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompanyResponse create(@Valid @RequestBody CreateCompanyRequest request) {
+        return companyService.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public CompanyResponse getById(@PathVariable Long id) {
+        return companyService.getById(id);
+    }
+
+    @GetMapping
+    public List<CompanyResponse> getAll() {
+        return companyService.getAll();
+    }
+}
